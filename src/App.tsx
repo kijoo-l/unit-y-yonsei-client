ac9ea4067d627b46dd4f338c2ab66f74389a7f58
kijoo <kijoo1128@yonsei.ac.kr>
2026-05-17 23:14:45 +0900

    feat: create reservation form page and waiting status card component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import BoothList from "./pages/booth/BoothList";
 import AvailableBoothListPage from "./pages/booth/reservation/AvailableBoothList";
 import BoothReservationDetailPage from "./pages/booth/reservation/BoothReservationDetailPage";
 import { InformationMainPage } from './pages/more/InformationMainPage';
+import WaitingFormPage from "./pages/booth/reservation/ReservationFormPage";
 
 
 
@@ -20,6 +21,7 @@ function App() {
             <Route path="/reservation" element={<AvailableBoothListPage />} />
             <Route path="/booths/:boothNumber" element={<BoothReservationDetailPage />} />
             <Route path="/more" element={<InformationMainPage />} />
+            <Route path="/reservation/:boothNumber" element={<WaitingFormPage />} />
           </Routes>
         </BrowserRouter>
       </div>
